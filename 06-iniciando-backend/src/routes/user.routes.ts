61e5875d75abafb8a77ec8d9a8170344401daa73
Willian17 <willianfernandes113@gmail.com>
2020-09-01 12:32:04 -0400

    upload avatar

--- 06-iniciando-backend/src/routes/user.routes.ts
+++ 06-iniciando-backend/src/routes/user.routes.ts
@@ -1,27 +1,54 @@
 import { Router } from 'express';
+import multer from 'multer'
 
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload'
 
 const usersRouter = Router();
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body
-
+    
     const createUser = new CreateUserService()
-
+    
     const user = await createUser.execute({
-        email,
-        name,
-        password
+      email,
+      name,
+      password
     })
-
+    
     delete user.password
-
+    
     return response.json(user);
   } catch (err) {
-      return response.status(400).json({ Error: err.message });
+    return response.status(400).json({ Error: err.message });
   }
 });
+
+usersRouter.patch('/avatar', 
+  ensureAuthenticated, 
+  upload.single('avatar'),
+  async (request, response)=> {
+    try {
+
+      const updateUserAvatar = new UpdateUserAvatarService()
+
+     const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename
+      })
+
+      delete user.password
+      
+      return response.json(user)
+    } catch (err) {
+      return response.status(400).json({ Error: err.message });
+    }
+  })
 
 export default usersRouter;
--- 06-iniciando-backend/src/server.ts
+++ 06-iniciando-backend/src/server.ts
@@ -3,11 +3,14 @@ import express from 'express';
 import routes from './routes';
 
 import './database'
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+routes.use('/files' , express.static(uploadConfig.directory))
+
 
 const porta = 3333
 app.listen(porta, () => console.log(`🚀️ Server listening of port ${porta}!`));
